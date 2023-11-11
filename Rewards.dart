4e435c48fc29db59284d0695c8a9854a1122c16d
JuicyJawmes <84299154+JuicyJawmes@users.noreply.github.com>
2023-11-11 07:12:21 -0600

    Update Rewards.dart

--- Rewards.dart
+++ Rewards.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'PointsManager.dart';
 
 class Rewards extends StatefulWidget {
   @override
   _RewardsState createState() => _RewardsState();
 }
-
 class _RewardsState extends State<Rewards> {
-  String _selectedSponsor = 'amazon';
+  String _selectedSponsor = 'Amazon';
   int _selectedPoints = 100;
+
 
   @override
   Widget build(BuildContext context) {
+    final pointsManager = Provider.of<PointsManager>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text("Redeem Rewards"),
@@ -23,15 +26,22 @@ class _RewardsState extends State<Rewards> {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Unwired Rewards', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-            SizedBox(height: 20),
-            Text('Current Balance:', style: TextStyle(fontSize: 18)),
-            Text('785 pts', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+    body: Padding(
+    padding: const EdgeInsets.all(16.0),
+    child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+    Text('Unwired Rewards', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+    SizedBox(height: 20),
+    Consumer<PointsManager>(
+    builder: (context, pointsManager, child) => Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+    Text('Current Balance:', style: TextStyle(fontSize: 18)),
+    Text('${pointsManager.currentPoints} pts', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+            ],
+    ),
+    ),
             SizedBox(height: 20),
             Column(
               children: [
@@ -45,7 +55,7 @@ class _RewardsState extends State<Rewards> {
             Text('Sponsor:', style: TextStyle(fontSize: 18)),
             DropdownButton<String>(
               value: _selectedSponsor,
-              items: ["amazon", "bestbuy", "playstore", "target"].map((String value) {
+              items: ["Amazon", "Bestbuy", "Playstore", "Target"].map((String value) {
                 return DropdownMenuItem<String>(
                   value: value,
                   child: Text(value),
@@ -75,39 +85,79 @@ class _RewardsState extends State<Rewards> {
             ),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, 'Rewardsloginscreen');              // Navigate to login screen
+              onPressed: () async {
+                // Check if the user can redeem points and get the success status
+                bool canRedeem = await Provider.of<PointsManager>(context, listen: false).canRedeemPoints(_selectedPoints);
+                if (canRedeem) {
+                  Navigator.pushNamed(context, 'Rewardsloginscreen'); // Navigate to the next screen if successful
+                } else {
+                  // If not enough points, show a snackbar or some alert to the user
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Not enough points to redeem this reward.'),
+                      backgroundColor: Colors.red,
+                    ),
+                  );
+                }
               },
               child: Text('Login to Collect Rewards'),
+            ),
+            Spacer(),
+            Divider(thickness: 1.5), // begin bottom row of buttons
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.settings),
+                      onPressed: () {
+                        Navigator.pushNamed(context, 'ConfigScreen');
+                      },
+                    ),
+                    SizedBox(height: 6),
+                    Text(
+                      'Settings',
+                      style: TextStyle(fontSize: 10),
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.compare),
+                      onPressed: () {},
+                    ),
+                    SizedBox(height: 6),
+                    Text(
+                      'Compare Usage',
+                      style: TextStyle(fontSize: 10),
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.card_giftcard),
+                      onPressed: () {},
+                    ),
+                    SizedBox(height: 6),
+                    Text(
+                      'Redeem',
+                      style: TextStyle(fontSize: 10),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ],
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Rewards'),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Config'),
-          BottomNavigationBarItem(icon: Icon(Icons.compare), label: 'TBD'),
-        ],
-        onTap: (int index) {
-          switch (index) {
-            case 0:
-              Navigator.pop(context,'RewardsScreen'); // Replace with your navigation or action logic
-
-              break;
-            case 1:
-              Navigator.pushNamed(context, 'ConfigScreen');              break;
-            case 2:
-              print("Friends tab pressed"); // Replace with your navigation or action logic
-              break;
-            default:
-              break;
-          }
-        },
-      ),
     );
   }
-
   Widget _buildRewardConversion(int points, String value) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,

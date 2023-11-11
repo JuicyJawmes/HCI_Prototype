6a4855468ae717da2f1a8bf39ff0e585661dafdd
JuicyJawmes <84299154+JuicyJawmes@users.noreply.github.com>
2023-11-11 07:09:28 -0600

    Create AppPoints.dart
    Assigns productive apps with points

---
+++ AppPoints.dart
@@ -0,0 +1,17 @@
+
+const Map<String, int> appsPoints = {
+  'WhatsApp': 10,
+  'Messenger': 20,
+  'Outlook': 50,
+  'Discord': 30,
+  'Slack': 50,
+  'LinkedIn': 20,
+  'Reddit': 10,
+  'Telegram': 10,
+  'Phone': 5,
+  'AppStore': 5,
+  'Handshake': 15,
+  'Blackboard': 15,
+  'Settings': 5,
+  // Add other apps and their points here
+};

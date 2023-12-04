27f64589dac16a277d8b41cb6765ab33febe2602
JuicyJawmes <84299154+JuicyJawmes@users.noreply.github.com>
2023-12-03 22:44:07 -0600

    Update AppPoints.dart

--- AppPoints.dart
+++ AppPoints.dart
@@ -2,9 +2,9 @@
 const Map<String, int> appsPoints = {
   'WhatsApp': 10,
   'Messenger': 20,
-  'Outlook': 50,
+  'Outlook': 30,
   'Discord': 30,
-  'Slack': 50,
+  'Slack': 30,
   'LinkedIn': 20,
   'Reddit': 10,
   'Telegram': 10,
@@ -14,4 +14,16 @@ const Map<String, int> appsPoints = {
   'Blackboard': 15,
   'Settings': 5,
   // Add other apps and their points here
+};
+
+const Map<String, int> appsDeductPoints = {
+  'Facebook': 10,
+  'Instagram': 30,
+  'Netflix': 10,
+  'Youtube': 10,
+  'DisneyPlus': 10,
+  'Twitter': 10,
+  'Snapchat': 10,
+  'TikTok': 10,
+  // Add more apps and points to deduct here
 };
